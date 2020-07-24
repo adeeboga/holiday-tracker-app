8376b3952cf9aeee59c41171d3393a5fb94cede6
adeboga <adeeboga@gmail.com>
2020-07-24 13:15:35 +0300

    modify

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RegisterService} from './register.service'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private registerService: RegisterService) { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(event){
+    event.preventDefault();
+    console.log(event);
+    var name = event.target.elements[0].value;
+    var email = event.target.elements[1].value;
+    var group = event.target.elements[2].value;
+    var password = event.target.elements[3].value;
+    // debugger;
+    this.registerService.registerUser(name, email, group, password);
+
   }
 
 }
